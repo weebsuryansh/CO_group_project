cf4f10c67e358744a05d6142935807d2f97f3d72
ShreyanshSeth26 <shreyansh22484@iiitd.ac.in>
2023-05-13 16:18:57 +0530

    example

--- assembler.py
+++ assembler.py
@@ -40,7 +40,7 @@ def opcode5(instruction):
     print("00100"+"0"+registers[instruction[1]]+"")
     #complete the print statement.
 
-def opcode6(instruction):
+def opcode(instruction):
     variable[instruction[1]]="001000"
 
 #space for opcodes of between
